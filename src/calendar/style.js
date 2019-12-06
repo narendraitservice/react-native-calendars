e5ddf946f101fa685eab5483b65ff7a8b38c9879
narendraitservice <41145594+narendraitservice@users.noreply.github.com>
2019-12-06 16:14:14 +0530

    Update style.js

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -16,7 +16,7 @@ export default function getStyle(theme={}) {
     },
     week: {
       marginTop: 7,
-      marginBottom: 7,
+      marginBottom: 5,
       flexDirection: 'row',
       justifyContent: 'space-around'
     },
